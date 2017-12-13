5036cbcfca9438343a127acd0490d123d50f1e29
Akashdeep Deb <deb2@illinois.edu>
2017-12-12 22:54:53 -0600

    changed mask

--- remSpecShad.m
+++ remSpecShad.m
@@ -1,25 +1,24 @@
-function [flag] = remSpecShad(imflash, imambient)
-    linflash = 0.299*imflash(:,:,1) + 0.587*imflash(:,:,2) + 0.114*imflash(:,:,3);
-    linambient = 0.299*imambient(:,:,1) + 0.587*imambient(:,:,2) + 0.114*imambient(:,:,3);
-    mask = linflash - linambient;
+function [mask] = remSpecShad(flash, ambient)
+    flash_lin = 0.299*flash(:,:,1) + 0.587*flash(:,:,2) + 0.114*flash(:,:,3);
+    ambient_lin = 0.299*ambient(:,:,1) + 0.587*ambient(:,:,2) + 0.114*ambient(:,:,3);
+    mask = flash_lin - ambient_lin;
     flag = zeros(size(mask));
-
-    thr1 = -0.05;
-    thr2 = -0.2;
-    flag(mask>thr2 & mask<thr1)=1;
-    flag(mask>0.65 & mask<0.7)=1;
-    rang = 0.95*(max(linflash(:))-min(linflash(:)));
-    flag(linflash>rang)=1;
+    flag(mask < -0.05 & mask > -0.2) = 1;
+    flag(mask > 0.65 & mask < 0.7) = 1;
+    spec = 0.95 * (max(flash_lin(:)) - min(flash_lin(:)));
+    flag(flash_lin > spec) = 1;
 
     %this part is required to get a better reading
-    se1 = strel('disk',2);
-    se2 = strel('disk',6);
-    se3 = strel('disk',4);
-    flag = imerode(flag,se1);
+    %idea from: https://github.com/agarwal-shubham/Digital-Photography-with-Flash-and-No-flash-Image-Pairs-/blob/master/codes/shadowRem.m
+    restruct1 = strel('disk',2);
+    restruct2 = strel('disk',6);
+    restruct3 = strel('disk',4);
+    
+    flag = imerode(flag,restruct1);
     flag = imfill(flag,'holes');
-    flag = imdilate(flag,se2);
-    flag = imerode(flag,se3);
+    flag = imdilate(flag,restruct2);
+    flag = imerode(flag,restruct3);
    
-    H=fspecial('gaussian',3,3);
-    flag = imfilter(flag,H);
+    %apply a gaussian filter
+    mask = imgaussfilt(flag,3);
 end
